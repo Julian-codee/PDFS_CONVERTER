831986cf7d0f58cc990a3cc4fe56dfd82e0ad7f3
Julian_codee <mmm808383@gmail.com>
2026-09-03 21:11:17 -0500

    Realizamos La conexion con pywebview, para hacer la comunicación entre el front y el back

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ import webview
 class API:
 
     def test_conection(self):
-        return "conexion exitosa"
+        return {
+            "ok": True,
+            "Message": 'Conexión Python > JavaScript Exitosa!'
+        }
+
+def getRoute_Front():
+    dir_base = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(dir_base, 'Frontend', 'index.html')
+
+if __name__ == "__main__":
+
+    api = API()
+
+    route_html = getRoute_Front()
+
+    window = webview.create_window(
+        "Conversor PDF",
+        route_html,
+        js_api=api,
+        width=1000,
+        height=800,
+        min_size=(800,600)
+    )
+
+    webview.start()
+
 
