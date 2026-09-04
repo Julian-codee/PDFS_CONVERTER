2be912faf36209447bf6245a5ba4080d52870518
Julian_codee <mmm808383@gmail.com>
2026-09-03 21:18:08 -0500

    Creamos la conexion js con python

---
+++ Frontend/app.js
@@ -0,0 +1,30 @@
+const boton = document.getElementById("btnProbar");
+const estado = document.getElementById("estado");
+
+
+boton.addEventListener("click", async () => {
+
+    try {
+
+        const respuesta =
+            await window.pywebview.api.test_conection();
+
+        console.log(respuesta);
+
+        if (respuesta.ok) {
+
+            estado.textContent =
+                respuesta.mensaje;
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        estado.textContent =
+            "Error de comunicación con Python.";
+
+    }
+
+});
