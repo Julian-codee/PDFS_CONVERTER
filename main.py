47ba8bfd20d28ea9998a916c030c916dd6fba875
Julian_codee <mmm808383@gmail.com>
2026-09-08 20:31:01 -0500

    Empezamos con la conexion del backend y hacemos ciertas validaciones que recibira el sistema

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 import os
 import webview
+import pymupdf
+
+from Backend.Redactor import (
+    convertir_pdf_a_imagenes,
+    validar_archivo_pdf,
+    ErrorConversorPDF
+)
 
 class API:
 
-    def test_conection(self):
-        return {
-            "ok": True,
-            "Message": 'Conexión Python > JavaScript Exitosa!'
-        }
+    def __init__(self):
+        self.window = None
+        self.pdf_actual = None
 
+## Seleccion del PDF
+
+    def selection_pdf(self):
+
+        try:
+            
+            archivos = self.window.create_file_dialog(
+                webview.OPEN_DIALOG,
+                allow_multiple=False,
+                file_types=(
+                    "Archivos PDF (*.pdf)",
+                    "*.pdf"
+                )
+            )
+
+            # El usuario cancelo 
+            if not archivos:
+                return{
+                    "Ok ":False,
+                    "Cancelado ":True
+                }
+
+            route_pdf = archivos[0]
+
+            # Validación dentro del backend
+
+
+        
+
+# pyrefly: ignore [parse-error]
 def getRoute_Front():
     dir_base = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(dir_base, 'Frontend', 'index.html')
